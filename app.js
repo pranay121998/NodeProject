d449bd57e1592f2d9e3a32dd375cda8181d73b79
nirmalgiri007@gmail.com <nirmalgiri007@gmail.com>
2022-01-06 12:26:35 +0530

    template engines

--- app.js
+++ app.js
@@ -4,20 +4,33 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const expressHbs = require('express-handlebars')
+const ejs = require('ejs');
+
+const adminData = require('./routes/admin');
 const shopRoutes =require('./routes/shop')
+
+// app.set('view engine',"pug");
+// app.set('views','views')
+
+// app.engine('hbs',expressHbs({layoutsDir: 'views/layouts/', defaultLayout:'main-layout',extname:'hbs' }));
+// app.set('view engine',"hbs");
+
+app.set('view engine','ejs');
+app.set('views','views')
 
 app.use(express.static(path.join(__dirname,"public")));
 
 app.use(bodyParser.urlencoded({extended : false}));
 
 
-app.use("/admin",adminRoutes);
+app.use("/admin",adminData.routes);
 
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,"views","404.html"));
+    // res.status(404).sendFile(path.join(__dirname,"views","404.html"));
+    res.status(404).render('404',{pageTitle:'Page Not Found'})
 })
 
 app.listen(8000)
--- routes/shop.js
+++ routes/shop.js
@@ -6,10 +6,20 @@ const router = express.Router();
 
 const rootPath = require('../utils/path')
 
+const adminData = require('./admin')
 
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootPath,"views","shop.html"));
-    // next()
+    // console.log("shop ",adminData.products)
+    // res.sendFile(path.join(rootPath,"views","shop.html"));
+    // // next()
+    const products =adminData.products;
+    res.render('shop',{prods: products,
+        pageTitle: 'Shop', 
+        path : '/', 
+        hasProduct :products.length >0,
+        activeShop:true,
+        productCSS:true
+    });
 })
 
 module.exports = router;
